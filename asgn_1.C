78a6f73db9c8af375b18ef155b4a8215c08fd199
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-04 15:05:58 +0530

    asgn_1.C

--- asgn_1.C
+++ asgn_1.C
@@ -1,13 +1,16 @@
 #include<stdio.h>
-intmain(){
+int main()
+{
     int year;
     printf("Enter a Year:");
     scanf("%d",&year);
 
 
-    if((year %4==0 && year%100!=0)|| (year% 400==0)){
+if((year %4==0 && year%100!=0)|| (year% 400==0))
+{
     printf("%d is a leap year.\n",year);}
-    else{
-     printf("%d is not a leap year.\n" , year);}
+       else{
+              printf("%d is not a leap year.\n" , year);
+           }
     return 0;
 }

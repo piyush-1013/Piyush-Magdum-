141914e9133cc88ae1427c7ab842ec314da24559
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-12 14:59:55 +0530

    asgn_2.c

--- asgn_2.c
+++ asgn_2.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
-int main() {
+void main() 
+{
     float a, b;
 
     printf("Enter two numbers: ");
-    scanf("%f %f", &a, &b);
+    scanf("%f%f", &a, &b);
 
     printf("\nAddition = %.2f", a + b);
     printf("\nSubtraction = %.2f", a - b);
     printf("\nMultiplication = %.2f", a * b);
 
-    if (b != 0) {
+    if (b != 0)
+    {
         printf("\nDivision = %.2f", a / b);
-    } else {
+    } else 
+    {
         printf("\nDivision not possible (division by zero)");
     }
-
-    return 0;
 }
+

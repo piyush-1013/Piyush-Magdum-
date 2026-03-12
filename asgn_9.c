f1e741792be4b9f1177a3f4ac57652b7e78801a5
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-12 15:25:50 +0530

    asgn_9.c

--- asgn_9.c
+++ asgn_9.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+void main() 
+{
+    int n, original, remainder, result = 0;
+    
+    scanf("%d", &n);
+    original = n;
+    
+    while (original != 0) 
+    {
+        remainder = original % 10;
+        result += remainder * remainder * remainder;
+        original /= 10;
+    }
+    
+    if (result == n)
+        printf("Armstrong number");
+    else
+        printf("Not an Armstrong number");
+}

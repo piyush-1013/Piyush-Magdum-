13b0e601e365b527fc159a4065abfb93d7742074
piyush-1013 <piyush.magdum10@gmail.com>
2026-03-29 14:13:43 +0530

    asgn_16.c

--- asgn_16.c
+++ asgn_16.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
+
 int main() {
-    int n;
-    printf("Enter the number of elements: ");
+    int n, i;
+    int arr[100];
+
+    printf("Enter number of elements: ");
     scanf("%d", &n);
-    int arr[20], even[20], odd[20];
-    int evenCount = 0, oddCount = 0;
-    printf("Enter %d integers: ", n);
-    for (int i = 0; i < n; i++) {
+
+    printf("Enter %d integers : ",n);
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n; i++) {
-        if (arr[i] % 2 == 0)
-            even[evenCount++] = arr[i];
-        else
-            odd[oddCount++] = arr[i];
+
+    printf("Even numbers are: ");
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
     }
-    printf("\nEven numbers: ");
-    if (evenCount == 0)
-        printf("None");
-    else
-        for (int i = 0; i < evenCount; i++)
-            printf("%d ", even[i]);
 
-    // Display odd numbers
-    printf("\nOdd numbers:  ");
-    if (oddCount == 0)
-        printf("None");
-    else
-        for (int i = 0; i < oddCount; i++)
-            printf("%d ", odd[i]);
+    printf("\nOdd numbers are: ");
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
 
-    printf("\n");
     return 0;
-}
+} 
